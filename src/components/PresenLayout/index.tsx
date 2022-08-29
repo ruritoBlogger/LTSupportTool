54003b2eac04d799dba5678b3b9929dff9c3eefe
ruritoBlogger <ruriro0125@gmail.com>
2022-08-29 23:40:06 +0900

    WIP: Layoutを少し整えた

--- src/components/PresenLayout/index.tsx
+++ src/components/PresenLayout/index.tsx
@@ -1,30 +1,39 @@
+import { css } from "@emotion/css";
 import { Grid } from "@mui/material";
-import { Box } from "@mui/system";
 
 const PresenLayout = (): JSX.Element => {
   return (
     <>
-      <Grid container direction={"column"}>
+      <Grid container direction={"column"} className={rootStyle}>
         <Grid container item xs={9}>
-          <Grid item xs={8}>
-            <Box>
-              <p>プレゼンの部分</p>
-            </Box>
+          <Grid item xs={9} className={boxStyle}>
+            <p>プレゼンの部分</p>
           </Grid>
-          <Grid item xs={4}>
-            <Box>
-              <p>サイドバーの部分</p>
-            </Box>
+          <Grid item xs={3} className={boxStyle}>
+            <p>サイドバーの部分</p>
           </Grid>
         </Grid>
-        <Grid item xs={3}>
-          <Box>
-            <p>下の部分</p>
-          </Box>
+        <Grid item xs={3} className={boxStyle}>
+          <p>下の部分</p>
         </Grid>
       </Grid>
     </>
   );
 };
+
+const rootStyle = css`
+  && {
+    height: 100%;
+    width: 100%;
+    background-color: black;
+  }
+`;
+
+const boxStyle = css`
+  && {
+    margin: 3px;
+    border: 3px solid white;
+  }
+`;
 
 export default PresenLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import PresenLayout from "@components/PresenLayout";
+import { css } from "@emotion/css";
 import { NextPage } from "next";
 import { SWRConfig } from "swr";
 
 const App: NextPage = () => {
   return (
     <SWRConfig value={{ suspense: true }}>
-      <PresenLayout />
+      <div className={rootStyle}>
+        <PresenLayout />
+      </div>
     </SWRConfig>
   );
 };
+
+const rootStyle = css`
+  && {
+    height: 100vh;
+    width: 100vw;
+  }
+`;
 
 export default App;
