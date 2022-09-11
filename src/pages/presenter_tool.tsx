279df8562548f4e9093fa8344c2fecd68926d326
ruritoBlogger <ruriro0125@gmail.com>
2022-09-11 11:01:43 +0900

    feat: 発表者ツールにタイマー関連のボタンを表示

--- src/pages/presenter_tool.tsx
+++ src/pages/presenter_tool.tsx
@@ -1,12 +1,22 @@
 import { css } from "@emotion/css";
+import { Button } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
 import { SWRConfig } from "swr";
 
 const PresenterToolPage: NextPage = () => {
+  const [message, setMessage] = useState<string>("");
+  // TODO: タイマーをスタートする
+  const handleClick = () => {
+    setMessage("clicked!!!");
+  };
+
   return (
     <SWRConfig value={{ suspense: true }}>
       <div className={rootStyle}>
         <p>発表者ツール</p>
+        <Button onClick={handleClick}>スタート</Button>
+        <p>{message}</p>
       </div>
     </SWRConfig>
   );
