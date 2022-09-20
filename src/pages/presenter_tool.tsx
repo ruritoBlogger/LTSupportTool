62329461f10e6f584730506221640a272cbcf384
ruritoBlogger <ruriro0125@gmail.com>
2022-09-20 20:01:36 +0900

    :sparkles: タイマーが再生中か否かを発表者ツール側で持つようにした。できればタイマー側で持ちたかった

--- src/pages/presenter_tool.tsx
+++ src/pages/presenter_tool.tsx
@@ -5,37 +5,36 @@ import { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { SWRConfig } from "swr";
 
-let socket: Socket | null = null;
-
 const PresenterToolPage: NextPage = () => {
-  const initializeSocket = async (): Promise<void> => {
-    if (socket) {
-      return;
-    }
-
-    await fetch("/api/socket");
-    socket = io();
-  };
+  const [isStart, setIsStart] = useState<boolean>(false);
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    initializeSocket();
+    const f = async () => {
+      await fetch("/api/socket");
+      setSocket(io());
+    };
+    f();
   }, []);
 
   const handleStartTimerClick = () => {
-    if (socket) {
+    if (socket && !isStart) {
       socket.emit("startTime", true);
+      setIsStart(true);
     }
   };
 
   const handleStopTimerClick = () => {
-    if (socket) {
+    if (socket && isStart) {
       socket.emit("stopTime", true);
+      setIsStart(false);
     }
   };
 
   const handleResetTimerClick = () => {
     if (socket) {
       socket.emit("resetTime", true);
+      setIsStart(false);
     }
   };
 
