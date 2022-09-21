e4e4acabf1600de2146f16c1c502efcc7eedf614
ruritoBlogger <ruriro0125@gmail.com>
2022-09-21 22:02:19 +0900

    :sparkles: urlを入力するフォームにzodを用いたバリデーションをかけるようにした

--- src/components/GoogleSlideForm/index.tsx
+++ src/components/GoogleSlideForm/index.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  url: z.string().url({
+    message:
+      "不正な値が入力されています。Google Slideのurlを入力してください。",
+  }),
+});
 
 const GoogleSlideForm = (): JSX.Element => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: zodResolver(schema) });
   const onSubmit = (data) => console.log(data);
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("url", { required: true })} />
-        {errors.url && <span>This field is required</span>}
+        <input {...register("url")} />
+        {/* FIXME: asで握り潰すのは良くないので何とかしたい */}
+        {errors.url?.message && <p>{errors.url?.message as string}</p>}
         <input type="submit" />
       </form>
     </>
